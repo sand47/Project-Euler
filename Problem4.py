a75037fea80bed8adcfb9c3f04f6de58e2db091c
Sandeep Kumar <sandeepkumaramni@gmail.com>
2017-04-08 08:56:02 +0530

    Update Problem4.py

--- Problem4.py
+++ Problem4.py
@@ -6,11 +6,11 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 By Sandeep kumar 1/4/17
 
 '''
-def is_pal(c):
+def is_pal(c):                                         # checking for palindrome 
     return int(str(c)[::-1]) == c
 
 maxpal = 0
-for a in range(999, 99, -1):
+for a in range(999, 99, -1):                           #takng value of a from 999 to 100
     for b in range(a, 99, -1):
         prod = a * b
         if is_pal(prod) and prod > maxpal:
